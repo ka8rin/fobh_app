6ea0bb7179ad7bb9901ca30de98321c021acc2dd
Katerina Charilaou <kc00156@surrey.ac.uk>
2012-01-08 02:58:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FobhApp::Application.config.secret_token = '055802f92d5eb8b6b3ea7090d2abb36b11a5f02b28df94082efd561bde8a652c3c0f2ed64ac23f5ffe9ab5ba0e0a1eba63bce168889e63851a97ca2961f31f5b'
